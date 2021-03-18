347b50569cd684df92cc2ef571685a7639b15908
LauraDee <ledee@umn.edu>
2021-03-18 11:58:21 -0600

    Cleaned up extra notes

--- code/master.R
+++ code/master.R
@@ -1,24 +1,7 @@
 ########################################################################
 ## NutNet Main Text Models ###########################################
-## Laura Dee - in LFE ###############################################
+## #######################################################################
 ##########################################################################
-# updated Dec 27, 2019
-#updated May 11, 2020 to include Simpsons div models 
-#cleaned up to focus on main results for Figures 2 & 3 on Feb 5 2021 
-
-#notes on using lfe versus previous implementation of models:
-#it does, especially if you're in a "FE nested within clusters" setting
-#which the code I'd given you before did not account for
-# since i was unaware of it at the time
-# basically when doing degrees of freedom corrections, lfe won't count fixed effects nested
-# in clusters b/c that would sort of be double counting
-
-#plotting coefficient estimates from felm objects:
-# https://raw.githack.com/uo-ec607/lectures/master/08-regression/08-regression.html#high_dimensional_fes_and_(multiway)_clustering
-
-#plotting resurces: http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
-# theme(legend.position="top") 
-#http://www.sthda.com/english/wiki/ggplot2-legend-easy-steps-to-change-the-position-and-the-appearance-of-a-graph-legend-in-r-software
 
 rm(list = ls())
 
