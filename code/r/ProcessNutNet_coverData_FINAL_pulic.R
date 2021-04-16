ff267811ae4046889b205403ece627a157ff0792
LauraDee <ledee@umn.edu>
2021-04-16 13:01:14 -0600

    Update ProcessNutNet_coverData_FINAL_pulic.R

--- code/r/ProcessNutNet_coverData_FINAL_pulic.R
+++ code/r/ProcessNutNet_coverData_FINAL_pulic.R
@@ -150,24 +150,12 @@ cover <- dplyr::left_join(cover,year0.sp) # joining the two dataframes together.
 
 # if the species isnt present at a site in year_trt == 0, give the species a relative abundance of 0 in that year:
 cover$ave_rel_abundance_year0[is.na(cover$ave_rel_abundance_year0)] = 0
+cover$rel_freq.space[is.na(cover$rel_freq.space)] = 0
 #check that it worked
 summary(cover$ave_rel_abundance_year0)
 
 hist(cover$ave_rel_abundance_over_time.live)
 summary(cover$ave_rel_abundance_over_time.live) 
-
-# delete the next three lines
-
-#cover[, ave_rel_abundance_year0 := ave_rel_abundance_over_time.live[year_trt == 0], by = .(Taxon, site_code)]
-
-# We compute relative abundance per species and site in pre-treatment year (year_trt == 0) and for LIVE cover
-# we use the pre-treatment year because we calculate the metrics at the site level and want to avoid classifying species post treatment
-# in case of spillover effects from the treated plots from nitrogen addition
-
-# delete the next two lines
-#cover[, rel_abundance_year0 := relative_sp_cover.yr.live[year_trt == 0], by = .(Taxon,  site_code)]
-#cover[, rel_abundance_year0 := ave_rel_abundance_over_time.live[year_trt == 0], by = .(Taxon,  site_code)]
-
 
 # see and plot the summary of these metrics 
 summary(cover$rel_freq.space)
@@ -183,16 +171,8 @@ plot(cover$rel_freq.space,cover$rel_abundance_year0)
 #######################################################################################################################
 ## Compute the DI per species per sie defined as:  DI = (average relative abundance + relative frequency)/2 #########
 ## considering live cover and pre-treatment year  ################################################################
+##############################################################################################################################
 cover[, DI := (ave_rel_abundance_year0 + rel_freq.space)/2 , by =.(Taxon, site_code)]
-
-
-# this yields a per species and per site DI variable. NA implies that the species was not found at the site in the 
-#pre-treatment year 
-#** figure this out -- length(unique(cover$Taxon[DI == "NA"]))
-cover[, countDINAs := length(unique(Taxon[DI == "NA"])), by =.(plot, site_code, year)]
-table(cover$site_code, cover$countDINAs)
-hist(cover$countDINAs)
-
 
 ## filter to only the live (the dead cover will be 0, which inflates the 0, bc of how we computed stuff above)
 hist(cover[live == 1,DI], xlab = "Dominance indicator (DI)", main = "Dominance indicator metric")
@@ -208,22 +188,17 @@ cover[, DIgroup:=cut(DI, breaks=c(0.0,0.2,0.8,1.0), labels=c("Rare","Subordinate
 cover[, sr_domspp := length(unique(Taxon[DIgroup == "Dominant"])), by = .(plot, site_code, year)]
 cover[, sr_rarespp := length(unique(Taxon[DIgroup == "Rare"])), by = .(plot, site_code, year)]
 cover[, sr_subordspp := length(unique(Taxon[DIgroup == "Subordinate"])), by = .(plot, site_code, year)]
-cover[, sr_NAspp := length(unique(Taxon[DIgroup == "NA"])), by = .(plot, site_code, year)]
 
 ### create a category that groups together all the non-rare
 cover[, sr_non_rare_spp := length(unique(Taxon[DIgroup %in% c("Subordinate", "Dominant")])), by = .(plot, site_code, year)]
 #create a rare grouping (including native and invasive)
 cover[, rareSR_spp.DI1 := length(unique(Taxon[DIgroup %in% c("Rare")])), by = .(plot, site_code, year)]
 
-#make a group and count with rare species based on year 0 and species that had a DI= 0 in year 0 so are NAs for the DIgroup
-cover[, rareSRNASR_spp.DI1 := length(unique(Taxon[DIgroup %in% c("Rare", "NA")])), by = .(plot, site_code, year)]
-
 summary(cover$sr_domspp) # max is 2 species that are dominant 
 summary(cover$sr_subordspp) 
 summary(cover$sr_rarespp) 
 summary(cover$sr_non_rare_spp)
-summary(cover$rareSRNASR_spp.DI1)
-summary(cover$sr_NAspp)
+
 
 # compute change in richness in each group 
 cover[order(year), change_sr_domspp := sr_domspp -shift(sr_domspp), by =.(plot, site_code)]
